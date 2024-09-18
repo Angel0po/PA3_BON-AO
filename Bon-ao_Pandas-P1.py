eaafd550b80c179e8f0305db81e0c345720327a6
Angelo <angelo.bonao.eng@ust.edu.ph>
2024-09-18 18:01:16 +0800

    Add files via upload

--- Bon-ao_Pandas-P1.py
+++ Bon-ao_Pandas-P1.py
@@ -1,19 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[123]:
+
+
 #start
 
 #Import library of PANDAS
 import pandas as pd
 
-#For part a.
-
 #Load csv file into a data frame named cars using pandas
 df_cars = pd.read_csv('cars.csv')
 
-#For part b.
+
+# In[125]:
+
 
 #use .head() to display the first five rows of the data frame of cars
 df_cars.head()
+
+
+# In[127]:
+
 
 #use .tail() to display the last five rows of the data frame of cars
 df_cars.tail()
 
 #end
+
