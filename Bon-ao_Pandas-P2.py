930c27caaa18e71604adfd14f7e0afaee1967aa9
Angelo <angelo.bonao.eng@ust.edu.ph>
2024-09-18 18:13:16 +0800

    Update Bon-ao_Pandas-P2.py

--- Bon-ao_Pandas-P2.py
+++ Bon-ao_Pandas-P2.py
@@ -6,18 +6,26 @@ import pandas as pd
 #Load csv file into a data frame named cars using pandas
 df_cars = pd.read_csv('cars.csv')
 
+# For part a.
+
 # The condition :5 inside the square bracket selects the first 5 rows of the DataFrame but does not include the upper range, 5th row, to be printed
 
 df_cars.iloc[:5,::2]
 
 # The double colon ::2 slices every two steps of the column
 
+# For part b.
+
 #Uses .loc to locate and print the row that has the key value : 'Model' is equal to 'Mazda RX4'
 df_cars.loc[df_cars['Model']=='Mazda RX4'] 
+
+# For part c.
 
 #Uses .loc to locate and print information that has the key value : 'Model' is equal to 'Mazda RX4'
 #This time the syntax next to locating the model is the specified info/column to be printed
 df_cars.loc[(df_cars['Model']=='Camaro Z28'), ['cyl']]
+
+# For part d.
 
 #Create list of car models needed for information
 car_models = ['Mazda RX4','Ford Pantera L','Honda Civic']
